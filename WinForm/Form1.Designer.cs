d32fddc6bcc19535a0a2b3ce590d17666600ba27
Ilgam <143835317+S7aryyx@users.noreply.github.com>
2026-05-11 19:41:56 +0500

    Добавьте файлы проекта.

---
+++ WinForm/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gb_connection = new GroupBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            textBox6 = new TextBox();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            gb_connection.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // gb_connection
+            // 
+            gb_connection.Controls.Add(textBox5);
+            gb_connection.Controls.Add(textBox4);
+            gb_connection.Controls.Add(textBox3);
+            gb_connection.Controls.Add(textBox2);
+            gb_connection.Controls.Add(textBox1);
+            gb_connection.Location = new Point(12, 12);
+            gb_connection.Name = "gb_connection";
+            gb_connection.Size = new Size(285, 170);
+            gb_connection.TabIndex = 0;
+            gb_connection.TabStop = false;
+            gb_connection.Text = "Connections";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 22);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "IP";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(6, 51);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "PORT";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(6, 80);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Database Title";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(6, 109);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "User";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(6, 138);
+            textBox5.Name = "textBox5";
+            textBox5.PasswordChar = '*';
+            textBox5.PlaceholderText = "Password";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Загрузить";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(313, 23);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(394, 23);
+            textBox6.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(713, 23);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Поиск";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(313, 63);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(475, 323);
+            dataGridView1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(313, 415);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 5;
+            button3.Text = "TXT";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(394, 415);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 6;
+            button4.Text = "EXCEL";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(713, 415);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 7;
+            button5.Text = "Очистить";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(textBox6);
+            Controls.Add(button1);
+            Controls.Add(gb_connection);
+            Name = "Form1";
+            Text = "Form1";
+            gb_connection.ResumeLayout(false);
+            gb_connection.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox gb_connection;
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Button button1;
+        private TextBox textBox6;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ WinForm/Form1.cs
@@ -0,0 +1,17 @@
+using System.Data;
+using System.Text; //Для TXT
+using Npgsql; //Для БД
+using OfficeOpenXml; //Для Excel (и вроде PDF)
+
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        public DataTable myTable;
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelPackage
+        }
+    }
+}
